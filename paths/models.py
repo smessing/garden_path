efc9b9df7a32310ae11547cebe5be0211cd6eea9
Samuel Messing <samuel.messing@gmail.com>
2011-02-23 23:47:27 -0500

    working on path models.

--- paths/models.py
+++ paths/models.py
@@ -12,30 +12,52 @@ title -- title of the garden path
 
 """
   auth_email = models.EmailField()
-  auth_name = models.CharField(max_length=200)
+  auth_fname = models.CharField(max_length=200)
+  auth_lname = models.CharField(max_length=200)
   nick = models.SlugField()
   pub_date = models.DateTimeField('date published')
   title = models.CharField(max_length=300) 
 
+  def _get_full_name(self):
+    return '%s %s' % (self.auth_fname, self.auth_lname)
+
+  full_name = property(_get_full_name)
+
   def __unicode__(self):
-    return self.name
+    return self.nick
 
   class Meta:
-    verbose_name_plural = 'paths'
+    verbose_name = 'garden path'
+    verbose_name_plural = 'garden paths'
     ordering = ['pub_date']
 
 class Blob(models.Model):
 """One step in a garden path.
 
 Fields:
-body -- text of the blob
 article -- url to wikipedia origin
+body -- text of the blob
 parent -- the garden path this blog comes from
-position -- position in the garden path
+position -- (automatically generated) position in path
 
 """ 
-
-  body = models.TextField()
   article = models.URLField()
+  body = models.TextField()
   parent = models.ForeignKey(Path)
-  position = models.IntegerField()
+  __position = models.IntegerField()
+
+  def _get_pos(self):
+    return self.__position
+
+  get_pos = property(_get_pos)
+
+  def _set_pos(self, x):
+    self.__position = x
+
+  set_pos = property(_set_pos)
+
+  def __unicode__(self);
+    return unicode(self.position)
+
+  class Meta:
+    ordering = ['parent','-position']
