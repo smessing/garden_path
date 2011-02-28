0b6bbfb46f072ff1e44db20714b47b43fff668b0
Samuel Messing <samuel.messing@gmail.com>
2011-02-27 20:21:42 -0500

    removing parts of models django doesn't like.

--- paths/models.py
+++ paths/models.py
@@ -1,18 +1,7 @@
 from django.db import models
 
 class Path(models.Model):
-"""A new garden path through wikipedia.
-
-Fields:
-approved -- boolean, only approved paths will be shown
-auth_email -- string, author's e-mail
-auth_name -- string author's name
-nick -- slug, a short nickname for url purposes
-pub_date -- datetime, date published
-title -- string, title of the garden path
-
-"""
-  __approval = False
+  approval = False
   auth_email = models.EmailField()
   auth_fname = models.CharField(max_length=200)
   auth_lname = models.CharField(max_length=200)
@@ -30,14 +19,6 @@ title -- string, title of the garden path
 
   auth_name = property(_get_full_name)
 
-  def _get_approval(self):
-    return self.__approved
-
-  def _set_approval(self, status):
-    self.__approved = status
-
-  approval = property(_get_approval, _set_approval)
-
   def __unicode__(self):
     return self.nick
 
@@ -47,33 +28,13 @@ title -- string, title of the garden path
     ordering = ['pub_date']
 
 class Blob(models.Model):
-"""One step in a garden path.
-
-Fields:
-article -- url to wikipedia origin
-body -- text of the blob
-parent -- the garden path this blog comes from
-position -- (automatically generated) position in path
-
-""" 
   article = models.URLField()
   body = models.TextField()
-  parent = models.ForeignKey(Path)
-  __position = models.IntegerField()
+  path = models.ForeignKey(Path)
+  position = models.IntegerField()
  
-  def __init__(self, *args, **kwargs):
-    self.position = kwargs.get('position', 0)
- 
-  def _get_pos(self):
-    return self.__position
-
-  def _set_pos(self, x):
-    self.__position = x
-
-  position = property(_get_pos, _set_pos)
-
-  def __unicode__(self);
+  def __unicode__(self):
     return unicode(self.position)
 
   class Meta:
-    ordering = ['parent','position']
+    ordering = ['path','position']
