ee93b9cc7e48e0aff2ca3def298b5a7e41464f4d
Samuel Messing <samuel.messing@gmail.com>
2011-02-27 20:31:45 -0500

    updating urls.

--- urls.py
+++ urls.py
@@ -8,9 +8,6 @@ urlpatterns = patterns('',
     # Example:
     # (r'^garden_path/', include('garden_path.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
