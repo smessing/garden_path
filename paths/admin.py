f1e083f0074d3cbb65b07d101bf6475e65dfdf5b
Samuel Messing <samuel.messing@gmail.com>
2011-02-27 21:23:41 -0500

    more admin interface changes.

--- paths/admin.py
+++ paths/admin.py
@@ -8,10 +8,13 @@ class BlobInLine(admin.TabularInline):
 class PathAdmin(admin.ModelAdmin):
   fieldsets = [
     ('Author',    {'fields':['auth_fname','auth_lname','auth_email']}),
-    # ('Approval Status', {'fields':['approval']}),
     ('Path Information', {'fields':['nick','title','pub_date']}),
   ]
   inlines = [BlobInLine]
+  list_display = ('title','pub_date','auth_name')
+  list_filter = ['pub_date']
+  search_fields = ['auth_name','pub_date','nick']
+  date_hierarchy = 'pub_date'
 
 admin.site.register(Path, PathAdmin)
 
