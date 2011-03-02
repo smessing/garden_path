c49422ef1727782dcecd2afa72ffd5abff310af6
Samuel Messing <samuel.messing@gmail.com>
2011-03-01 23:07:42 -0500

    figuring out how to display blobs.

--- paths/views.py
+++ paths/views.py
@@ -1,10 +1,12 @@
-from paths.models import Path
-from django.shortcuts import render_to_response, get_object_or_404
+from paths.models import Path, Blob
+from django.shortcuts import render_to_response, get_object_or_404,\
+  get_list_or_404
 
 def index(request):
   paths = Path.objects.all()
   return render_to_response('paths/index.html', {'paths':paths})
 
 def detail(request, path_nick):
-  path = get_object_or_404(Path, nick=path_nick)
-  return render_to_response('paths/detail.html', {'path':path})
+  _path = get_object_or_404(Path, nick=path_nick)
+  blobs = get_list_or_404(Blob, path=_path)
+  return render_to_response('paths/detail.html', {'path':_path, 'blobs':blobs})
