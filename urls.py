2acbf434e46fe03b5f82e603f1c37b13c0785f67
Samuel Messing <samuel.messing@gmail.com>
2011-03-13 18:38:09 -0400

    fixed style sheet issues, starting to mess with overall site look.

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^garden_path/index', 'paths.views.index'),
-    (r'^garden_path/(?P<path_nick>\w+)/$', 'paths.views.detail')
+    (r'^garden_path/(?P<path_nick>\w+)/$', 'paths.views.detail'),
     (r'^static_media/(?P<path>.*)$', 'django.views.static.serve', \
       {'document_root': settings.STATIC_DOC_ROOT})
 )
